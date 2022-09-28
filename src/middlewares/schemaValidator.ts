91fe6bf721dc03a19d41c13c4e598c8f38a9568e
Isadora Ávila <isadorgravila@gmail.com>
2022-09-28 12:44:57 -0300

    feat: create schemaValidator middleware

---
+++ src/middlewares/schemaValidator.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import { ObjectSchema } from "joi";
+
+export default function validateSchema(schema: ObjectSchema) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req.body, {abortEarly: false});
+        if (error) {
+            throw {code: 'schema_error', message: error.details.map((detail: { message: any }) => detail.message)}
+        }
+
+        next();
+    }
+}
