c613ea043320901997774bdaaf2a040e76fe316c
Isadora Ávila <isadorgravila@gmail.com>
2022-09-28 12:43:41 -0300

    feat: create errorHandler middleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function errorHandler (error: any, req: Request, res: Response, next: NextFunction) {
+  if (error.code === 'schema_error') {
+    return res.status(422).send(error.message);
+  }
+  if (error.code === 'unauthorized_error') {
+    return res.status(401).send(error.message);
+  }
+  if (error.code === 'notfound_error') {
+    return res.status(404).send(error.message);
+  }
+  if (error.code === 'conflict_error') {
+    return res.status(409).send(error.message);
+  }
+  return res.sendStatus(500);
+}
