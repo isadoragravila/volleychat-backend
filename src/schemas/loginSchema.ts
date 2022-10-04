680a9d5341c9d59f6baf220c7625f61be12aecd2
Isadora Ávila <isadorgravila@gmail.com>
2022-10-04 16:15:41 -0300

    feat: pattern in login and register schema

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -2,8 +2,8 @@ import joi from 'joi';
 import { ILoginData } from '../types/authTypes';
 
 const authSchema = joi.object<ILoginData>({
-    username: joi.string().required(),
-    password: joi.string().min(8).required(),
+    username: joi.string().pattern(/^[a-zA-Z0-9._-]*$/).required(),
+    password: joi.string().min(8).pattern(/^\S*$/).required()
   });
 
   export default authSchema;
--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -2,9 +2,9 @@ import joi from 'joi';
 import { IRegisterData } from '../types/authTypes';
 
 const authSchema = joi.object<IRegisterData>({
-    username: joi.string().required(),
+  username: joi.string().pattern(/^[a-zA-Z0-9._-]*$/).required(),
     email: joi.string().email().required(),
-    password: joi.string().min(8).required(),
+    password: joi.string().min(8).pattern(/^\S*$/).required(),
     image: joi.string().uri().required(),
     bio: joi.string().max(150).required()
   });
