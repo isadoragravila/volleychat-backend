eb8e8380f880b784b3c09629cc299e36f79d824f
Isadora Ávila <isadorgravila@gmail.com>
2022-10-04 23:41:14 -0300

    refactor: create variable agent for supertest(app)

--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -6,11 +6,13 @@ import { registerBody, wrongDataBody } from '../factories/dataFactory';
 
 beforeEach(async () => {
     await deleteAllData();
-  });
+});
+
+const agent = supertest(app);
 
 describe('POST /sign-up', () => {
     it('returns 422 for invalid input', async () => {
-        const result = await supertest(app).post('/sign-up').send({});
+        const result = await agent.post('/sign-up').send({});
 
         expect(result.status).toBe(422);
     });
@@ -18,22 +20,22 @@ describe('POST /sign-up', () => {
     it('returns 409 for using an existing username or email in the database', async () => {
         const body = await registerBody();
 
-        await supertest(app).post('/sign-up').send(body);
+        await agent.post('/sign-up').send(body);
 
-        const result = await supertest(app).post('/sign-up').send(body);
+        const result = await agent.post('/sign-up').send(body);
 
         expect(result.status).toBe(409);
     });
 
     it('returns 201 for valid input and right insert in the database', async () => {
         const body = await registerBody();
-        
-        const result = await supertest(app).post('/sign-up').send(body);
 
-        const createdUser = await prisma.users.findUnique({ 
+        const result = await agent.post('/sign-up').send(body);
+
+        const createdUser = await prisma.users.findUnique({
             where: { username: body.username }
         });
-        
+
         expect(result.status).toBe(201);
         expect(createdUser).not.toBeNull;
     });
@@ -43,9 +45,9 @@ describe('POST /sign-in', () => {
     it('returns 422 for invalid input', async () => {
         const body = await registerBody();
 
-        await supertest(app).post('/sign-up').send(body);
+        await agent.post('/sign-up').send(body);
 
-        const result = await supertest(app).post('/sign-in').send({});
+        const result = await agent.post('/sign-in').send({});
 
         expect(result.status).toBe(422);
     });
@@ -53,17 +55,17 @@ describe('POST /sign-in', () => {
     it('returns 401 for wrong credentials', async () => {
         const body = await registerBody();
 
-        await supertest(app).post('/sign-up').send(body);
+        await agent.post('/sign-up').send(body);
 
         const wrongBody = await wrongDataBody();
 
-        const firstTry = await supertest(app).post('/sign-in').send({ 
+        const firstTry = await agent.post('/sign-in').send({
             username: wrongBody.username,
             password: body.password
-        }); 
+        });
         expect(firstTry.status).toBe(401);
 
-        const secondTry = await supertest(app).post('/sign-in').send({ 
+        const secondTry = await agent.post('/sign-in').send({
             username: body.username,
             password: wrongBody.password
         });
@@ -72,13 +74,13 @@ describe('POST /sign-in', () => {
 
     it('returns 200 for valid input and right credentials', async () => {
         const body = await registerBody();
-        await supertest(app).post('/sign-up').send(body);
+        await agent.post('/sign-up').send(body);
 
-        const result = await supertest(app).post('/sign-in').send({ 
+        const result = await agent.post('/sign-in').send({
             username: body.username,
             password: body.password
         });
-        
+
         expect(result.status).toBe(200);
         expect(result.body).toHaveProperty('token');
     });
