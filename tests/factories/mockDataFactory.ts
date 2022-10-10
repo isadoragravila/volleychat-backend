9fb7364e0ed90d8e5bc2a49cf0c9534bc42e2b71
Isadora Ávila <isadorgravila@gmail.com>
2022-10-10 14:56:18 -0300

    test: unit: tests messageService

--- tests/factories/mockDataFactory.ts
+++ tests/factories/mockDataFactory.ts
@@ -1,4 +1,4 @@
-import { chatBody } from "./dataFactory";
+import { chatBody, messageBody, registerBody } from "./dataFactory";
 
 export async function chatData(id?: number) {
 	const chat = await chatBody();
@@ -26,9 +26,44 @@ export async function getChatsData() {
 
 	const chatrooms = [];
 	for (let i = 0; i < 3; i++) {
-		const chat = await chatData(i+1);
+		const chat = await chatData(i + 1);
 		chatrooms.push(chat);
 	}
 
 	return { ...category, chatrooms };
+}
+
+export async function messageData(id?: number) {
+	const message = await messageBody();
+
+	return {
+		id: id || 1,
+		content: message.content,
+		chatroomId: 1,
+		userId: 1,
+		createdAt: new Date()
+	};
+}
+
+export async function getMessagesData() {
+	const chat = await chatData();
+	const { content } = await messageBody();
+	const { username } = await registerBody();
+
+	return {
+		id: chat.id,
+		title: chat.title,
+		messages: [
+			{
+				id: 1,
+				content,
+				chatroomId: chat.id,
+				userId: 1,
+				createdAt: new Date(),
+				user: {
+					username
+				}
+			}
+		]
+	};
 }
---
+++ tests/unit/unitMessage.test.ts
@@ -0,0 +1,64 @@
+import * as messageService from "../../src/services/messageService";
+import * as chatRepository from "../../src/repositories/chatRepository";
+import * as messageRepository from "../../src/repositories/messageRepository";
+import { messageBody } from "../factories/dataFactory";
+import { chatData, getMessagesData, messageData } from "../factories/mockDataFactory";
+
+beforeEach(() => {
+	jest.resetAllMocks();
+	jest.clearAllMocks();
+});
+
+describe("Tests message creation", () => {
+	it("Should send a new message", async () => {
+		const message = await messageBody();
+		const userId = 1;
+		const chatroomId = 1;
+		const chatroom = await chatData();
+		const messageDone = await messageData();
+
+		jest.spyOn(chatRepository, "findById").mockResolvedValueOnce(chatroom);
+
+		jest.spyOn(messageRepository, "insert").mockResolvedValueOnce(messageDone);
+
+		await messageService.createMessage({ ...message, chatroomId, userId});
+
+		expect(chatRepository.findById).toBeCalledTimes(1);
+		expect(messageRepository.insert).toBeCalledTimes(1);
+	});
+
+	it("Shouldn't create a new chat, because category doesn't exists", async () => {
+		const message = await messageBody();
+		const userId = 1;
+		const chatroomId = 1000;
+
+		jest.spyOn(chatRepository, "findById").mockResolvedValueOnce(null);
+
+		jest.spyOn(messageRepository, "insert");
+
+		const result = messageService.createMessage({ ...message, chatroomId, userId});
+
+		await expect(result).rejects.toEqual({ code: "notfound_error", message: "Chatroom not found" });
+
+		expect(chatRepository.findById).toBeCalledTimes(1);
+		expect(messageRepository.insert).not.toBeCalled();
+	});
+});
+
+describe("Tests get chats by category", () => {
+	it("Should return an object with an array of messages", async () => {
+		const messages = await getMessagesData();
+		const chatroom = await chatData();
+		const userId = 1;
+
+		jest.spyOn(chatRepository, "findById").mockResolvedValueOnce(chatroom);
+
+		jest.spyOn(messageRepository, "findByChatId").mockResolvedValueOnce(messages);
+
+		const result = await messageService.getMessages(chatroom.id, userId);
+
+		expect(chatRepository.findById).toBeCalledTimes(1);
+		expect(messageRepository.findByChatId).toBeCalledTimes(1);
+		expect(result).toHaveProperty("userId");
+	});
+});
